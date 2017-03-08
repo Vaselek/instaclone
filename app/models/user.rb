15125a654f1e146c8c00943d169a1c1c5a51542c
Asel Burzhubaeva <burzhuas@gmail.com>
2017-03-09 05:11:29 +0600

    Added Photo model and Paperclip gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
+	has_many :photos, dependent: :destroy
 	has_many :active_relationships, class_name: "Relationship",
 																	foreign_key: "follower_id",
 																	dependent: :destroy
 	has_many :passive_relationships, class_name: "Relationship",
-																	 foreign_key: "followed_id"
+																	 foreign_key: "followed_id",
 																	 dependent: :destroy
 	has_many :following, through: :active_relationships,  source: :followed
 	has_many :followers, through: :passive_relationships, source: :follower
